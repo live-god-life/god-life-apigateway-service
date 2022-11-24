d88461e2d83a5050b0db7f12dca1630d2a9c9984
ss-hoon <lshoon1128@naver.com>
2022-11-24 22:16:57 +0900

    feat: 토큰 관련 에러 response 추가

--- src/main/java/com/godlife/apigatewayservice/filter/AuthorizationFilter.java
+++ src/main/java/com/godlife/apigatewayservice/filter/AuthorizationFilter.java
@@ -1,22 +1,27 @@
 package com.godlife.apigatewayservice.filter;
 
-import com.godlife.apigatewayservice.response.ApiResponse;
-import com.godlife.apigatewayservice.utils.JwtUtils;
-
-import lombok.extern.slf4j.Slf4j;
+import java.nio.charset.StandardCharsets;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ServerWebExchange;
 
+import com.godlife.apigatewayservice.response.ApiResponse;
+import com.godlife.apigatewayservice.utils.JwtUtils;
+import com.google.gson.Gson;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Component
@@ -39,6 +44,8 @@ public class AuthorizationFilter extends AbstractGatewayFilterFactory<Authorizat
 	public static class Config {
 	}
 
+
+
 	/**
 	 * 필터 실행 로직
 	 * @param config    설정 내용
@@ -46,10 +53,11 @@ public class AuthorizationFilter extends AbstractGatewayFilterFactory<Authorizat
 	 */
 	@Override
 	public GatewayFilter apply(Config config) {
+
 		return (((exchange, chain) -> {
 			ServerHttpRequest request = exchange.getRequest();
 
-			String jwt = JwtUtils.getToken(request);
+			String jwt = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0).replace("Bearer ", "");
 
 			// 테스트 진행용 ========================================
 			if ("test".equals(jwt)) {
@@ -63,13 +71,13 @@ public class AuthorizationFilter extends AbstractGatewayFilterFactory<Authorizat
 			// 테스트 진행용 ========================================
 
 			// 헤더에 토큰이 없는 경우
-			if (!StringUtils.hasText(jwt)) {
-				return onError(exchange, "No authorization header", HttpStatus.UNAUTHORIZED);
+			if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+				return onError(exchange, chain, 100, "토큰이 누락되었습니다.");
 			}
 
 			// 토큰이 유효하지 않는 경우
 			if (!JwtUtils.isJwtValid(jwt)) {
-				return onError(exchange, "JWT token is not valid", HttpStatus.UNAUTHORIZED);
+				return onError(exchange, chain, 101, "토큰이 유효하지 않습니다.");
 			}
 
 			// 토큰에서 사용자 정보 추출
@@ -87,12 +95,12 @@ public class AuthorizationFilter extends AbstractGatewayFilterFactory<Authorizat
 				.header("x-user", userId)
 				.retrieve()
 				.bodyToMono(ApiResponse.class)
-				.onErrorReturn(new ApiResponse<>())
+				.onErrorReturn(new ApiResponse())
 				.block()
 				.getData();
 
 			if (responseData == null) {
-				return onError(exchange, "This request is not a user.", HttpStatus.UNAUTHORIZED);
+				return onError(exchange, chain, 102, "회원이 아닙니다.");
 			}
 
 			// Request 헤더에 사용자 정보 추가
@@ -107,19 +115,20 @@ public class AuthorizationFilter extends AbstractGatewayFilterFactory<Authorizat
 	/**
 	 * 실패 시 처리 로직
 	 * @param exchange          Http 요청-응답 관련 속성
-	 * @param errorMessage      에러 메시지
-	 * @param httpStatus        Http 상태 코드
+	 * @param chain          	Filter Chain
+	 * @param code      		에러 코드
+	 * @param message        	에러 메시지
 	 * @return 에러 Response
 	 */
-	private Mono<Void> onError(ServerWebExchange exchange, String errorMessage, HttpStatus httpStatus) {
+	private Mono<Void> onError(ServerWebExchange exchange, GatewayFilterChain chain, int code, String message) {
 		ServerHttpResponse response = exchange.getResponse();
-		response.setStatusCode(httpStatus);
+		response.setStatusCode(HttpStatus.UNAUTHORIZED);
 
-		if (log.isErrorEnabled()) {
-			log.error("Error code: {}", httpStatus);
-			log.error("Error message: {}", errorMessage);
-		}
+		Gson gson = new Gson();
+		ApiResponse<?> body = ApiResponse.createErrorCode("error", code, message);
 
-		return response.setComplete();
+		DataBuffer dataBuffer = response.bufferFactory().wrap(gson.toJson(body).getBytes(StandardCharsets.UTF_8));
+
+		return response.writeWith(Flux.just(dataBuffer));
 	}
 }
--- src/main/java/com/godlife/apigatewayservice/response/ApiResponse.java
+++ src/main/java/com/godlife/apigatewayservice/response/ApiResponse.java
@@ -1,8 +1,11 @@
 package com.godlife.apigatewayservice.response;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class ApiResponse<T> {
 	/** 상태 (success / error) */
 	private String status;
@@ -15,4 +18,15 @@ public class ApiResponse<T> {
 
 	/** 오류 메시지 */
 	private String message;
+
+	private ApiResponse(String status, T data, Integer code, String message) {
+		this.status = status;
+		this.data = data;
+		this.code = code;
+		this.message = message;
+	}
+
+	public static ApiResponse createErrorCode(String status, Integer code, String message) {
+		return new ApiResponse<>(status, null, code, message);
+	}
 }
